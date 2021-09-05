677e1ad46ca508be05c68484cbb1ec0757ce900e
st21dy <st21dy@gmail.com>
2021-09-05 13:15:28 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :articles, dependent: :destroy
-  
+  attachment :icon
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2ef81232c7e630579c6eb714ee6a0216aaff4133e35461a3d56a32ccb205352b74cad1cbd43cbf7929d21836a12be9f7a2233fcb397fd8d41bb763f24846bff8'
