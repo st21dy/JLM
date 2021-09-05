6e08668fb7718198f5eca0b044536964d9fff272
st21dy <st21dy@gmail.com>
2021-09-05 07:58:24 +0000

    First Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy_user_session_path
+    
+  end
+  
+  
+end
---
+++ db/migrate/20210905074806_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :image_id
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
