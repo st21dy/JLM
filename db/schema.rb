59eeefa81f91d1e44de9284bd71ec40d9306918e
st21dy <st21dy@gmail.com>
2021-09-05 07:56:13 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_041917) do
+ActiveRecord::Schema.define(version: 2021_09_05_074806) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "image_id"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
