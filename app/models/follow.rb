c63583eee05d872064a9a3b3db2cb6dbd2c7025f
st21dy <st21dy@gmail.com>
2021-09-10 09:01:54 +0000

    First Commit

---
+++ app/models/follow.rb
@@ -0,0 +1,8 @@
+class Follow < ApplicationRecord
+ 
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  # class_name: "User"を定義することでUserテーブルのレコードを参照する
+  
+  
+end
