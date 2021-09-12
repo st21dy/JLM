eedd2e33a058b186117b942e4d9a6d98d8e63ef1
st21dy <st21dy@gmail.com>
2021-09-12 09:16:21 +0000

    First Commit

---
+++ app/controllers/article_comments_controller.rb
@@ -0,0 +1,30 @@
+class ArticleCommentsController < ApplicationController
+  
+  def create
+    article = Article.find(params[:article_id])
+    comment = current_user.article_comments.new(article_comment_params)
+    # comment = PostComment.new(post_comment_params)
+    # comment.user_id = current_user.id
+    comment.article_id = article.id
+    comment.save
+    redirect_to article_path(article)
+    
+  end
+  
+  def destroy
+    
+    article = Article.find(params[:article_id])
+    article_comment = current_user.article_comments.find_by(article_id: article.id)
+    article_comment.destroy
+    redirect_to article_path(article)
+    
+  end
+  
+  private
+
+  def article_comment_params
+    params.require(:article_comment).permit(:comment)
+  end
+  
+
+end
---
+++ app/models/tag_map.rb
@@ -0,0 +1,10 @@
+class TagMap < ApplicationRecord
+
+  belongs_to :article
+  belongs_to :tag
+
+  validates :article_id, presence: true
+  validates :tag_id, presence: true
+  
+  
+end
