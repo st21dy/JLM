78c5535060af5b16a3b067b9e3f16d4c54f91e85
st21dy <st21dy@gmail.com>
2021-09-28 15:03:23 +0000

    update

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,5 +1,4 @@
 class ArticleCommentsController < ApplicationController
-  
   def create
     article = Article.find(params[:article_id])
     comment = current_user.article_comments.new(article_comment_params)
@@ -8,23 +7,18 @@ class ArticleCommentsController < ApplicationController
     comment.article_id = article.id
     comment.save
     redirect_to article_path(article)
-    
   end
-  
+
   def destroy
-    
     article = Article.find(params[:article_id])
     article_comment = current_user.article_comments.find_by(article_id: article.id)
     article_comment.destroy
     redirect_to article_path(article)
-    
   end
-  
+
   private
 
   def article_comment_params
     params.require(:article_comment).permit(:comment)
   end
-  
-
 end
